6d11216f7fabac8dd98afa6004e71541efca736a
Nazrulislam991 <nazrulislamnayon991@gmail.com>
2025-11-26 15:37:26 +0600

    update

---
+++ lib/app/urls.dart
@@ -0,0 +1,8 @@
+class Urls {
+  static const String _baseUrl = 'https://api.brevo.com/v3';
+  static const String sendEmailOtp = '$_baseUrl/smtp/email';
+  static const String baseWeatherUrl =
+      'https://api.openweathermap.org/data/2.5/weather';
+  static const String calendarificBaseUrl =
+      "https://calendarific.com/api/v2/holidays";
+}
