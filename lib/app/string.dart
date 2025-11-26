7eb13235baa6d39887233f750fd28d8fe54f20d2
Nazrulislam991 <nazrulislamnayon991@gmail.com>
2025-11-26 11:11:06 +0600

    Remove sensitive string.dart from tracking

--- lib/app/string.dart
+++
@@ -1,9 +0,0 @@
-class AppConstants {
-  static const String adminEmail = "rentrover2025@gmail.com";
-  static const String cloudName = "";
-  static const String uploadPreset = "Bikretaa";
-  static const String brevoApiKey = "";
-  static const String weatherApiKey = "";
-  static const String calendarificApiKey = "";
-  static const String countryCode = "BD";
-}
