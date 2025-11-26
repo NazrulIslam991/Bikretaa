87165a1d2cecd536e165cf9a38b299af139149b2
Nazrulislam991 <nazrulislamnayon991@gmail.com>
2025-11-26 11:13:57 +0600

    Remove sensitive urls.dart from tracking

--- lib/app/urls.dart
+++
@@ -1,8 +0,0 @@
-class Urls {
-  static const String _baseUrl = 'https://api.brevo.com/v3';
-  static const String sendEmailOtp = '$_baseUrl/smtp/email';
-  static const String baseWeatherUrl =
-      'https://api.openweathermap.org/data/2.5/weather';
-  static const String calendarificBaseUrl =
-      "https://calendarific.com/api/v2/holidays";
-}
